08dce5b8827fa282dbdfee8e680209ec75f7d4c7
tucno21 <carlitostucno@gmail.com>
2022-08-27 21:20:46 -0500

    coneccion a la BD

--- System/Autoload.php
+++ System/Autoload.php
@@ -1,5 +1,7 @@
 <?php
 
+use System\ConnectDB;
+
 /**
  * cargar el autoloader de composer
  */
@@ -12,3 +14,11 @@ require_once dirname(__DIR__) . '/App/Config/App.php';
  * envia todas las funciones o helpers a la aplicacion
  */
 require_once 'Functions.php';
+
+/**
+ * obtener la configuracion de la aplicacion
+ * para la base de datos
+ */
+ConnectDB::dataBase($db_host, $db_user, $db_pass, $db_name, $db_charset);
+$database = ConnectDB::conn();
+dd($database);
