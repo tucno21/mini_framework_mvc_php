72a860742df4eb1148731f271c79d58376e9d451
tucno21 <carlitostucno@gmail.com>
2022-09-07 11:59:41 -0500

    model roles y permisos

---
+++ App/Model/Permissions.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Model;
+
+use System\Model;
+
+class Permissions extends Model
+{
+    /**
+     * nombre de la tabla
+     */
+    protected static $table       = 'permisos';
+    /**
+     * nombre primary key
+     */
+    protected static $primaryKey  = 'id';
+    /**
+     * nombre de la columnas de la tabla
+     */
+    protected static $allowedFields = ['per_name', 'description'];
+    /**
+     * obtener los datos de la tabla en 'array' u 'object'
+     */
+    protected static $returnType     = 'object';
+    /**
+     * puede eliminar si desea estos campos si no lo rquiere
+     * si desea que el framework registre las fechas para la creacion o  actualizacion
+     */
+    protected static $useTimestamps   = true;
+    /**
+     * $createdField debe ser DATETIME o TIMESTAMPS con condicion null
+     * $$updatedField debe ser TIMESTAMPS con condicion null
+     * el framework se encarga de enviar las fechas y no BD
+     * colocar el nombre de los campos de fecha de la BD
+     */
+    protected static $createdField    = 'created_at';
+    protected static $updatedField    = 'updated_at';
+}
